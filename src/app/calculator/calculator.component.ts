96ab7c1a4b460f99b686829c40a1a0d377039393
HelgaEn <feneshu@rambler.ru>
2022-11-28 13:53:16 +0300

    ver2

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public y =0;
+
+  changeNumber(y:number) {
+     this.y=y+1;
+     return y;
+  }
+}
