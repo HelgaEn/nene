45842c29a29c9e818fa11da6127e068ee464ee22
HelgaEn <feneshu@rambler.ru>
2023-01-20 12:40:09 +0300

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Book {
+  name: string | null;
+  author: string | null;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstEvoApp';
+  books: Book[] = [
+    {
+      name: 'Числа',
+      author: 'Пелевин'
+    },
+    {
+      name: 'Бездна',
+      author: 'Андреев'
+    }
+  ]
+
+  new_book = {
+    name: null,
+    author: null
+  }
+
+  newBook() {
+    this.books.push({
+      name: this.new_book.name,
+      author: this.new_book.author,
+    });
+  }
 }
