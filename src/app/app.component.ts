aac4d998befa7e64ddd98addde5573209056d1e0
HelgaEn <feneshu@rambler.ru>
2023-01-31 13:02:43 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { map, Observable, Subscription } from 'rxjs';
+import {range, timer, interval} from 'rxjs';
+//import {scan, map} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { __values } from 'tslib';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstEvoApp';
+  
+
+  intervalSub$!: Subscription;
+  intervalTub$!: Subscription;
+
+ constructor(){
+  
+ }
+
+ OnButtStart(){
+  const intervalStream = interval(2000);
+  this.intervalSub$=intervalStream.subscribe((value)=>{
+    const canvas = document.getElementById('frst1') as HTMLElement;
+    canvas.textContent = `${value}`;
+  })
+
+  const intervalStream2 = interval(2000);
+  this.intervalTub$=intervalStream2.pipe(map
+    (value => `Random value:  ${Math.floor(value*Math.random()*100)}`)).subscribe((value)=>{
+      const canvas2 = document.getElementById('scnd2') as HTMLElement;
+      canvas2.textContent = `${value}`;
+  })
+ }
+
+ 
+OnButtStop(){
+  this.intervalSub$.unsubscribe();
+}
+OnButtStop2(){
+  this.intervalTub$.unsubscribe();
+}
+ 
+
 }
