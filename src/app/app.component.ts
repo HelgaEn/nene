d9fe2b1cb15f9446758698f50b3578ee94890de6
HelgaEn <feneshu@rambler.ru>
2023-02-20 22:53:49 +0300

    isprav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,31 +14,12 @@ import { ArbusComponent } from './arbus/arbus.component';
 })
 
 export class AppComponent {
-  /*
- // @ViewChild('dynamicComp',{read: ViewContainerRef}) viev?: ViewContainerRef;
- //@ViewChild('dynamicComp', { read: ViewContainerRef }) private viev?: ViewContainerRef;
- //@ViewChild('dynamicComp') viev: ElementRef;
-  
-constructor(private cfr: ComponentFactoryResolver){}
-
-  async Load(){
-    const{ArbusComponent}=await import('./arbus/arbus.component');
-    const ArbusComponentFactory=this.cfr.resolveComponentFactory(ArbusComponent);
-    this.viev.clear();
-    //this.viev.createComponent(ArbusComponentFactory);
-    this.viev.createComponent(ArbusComponentFactory)
-    
-  }
-  deleteComp(){
-    this.viev.clear()
-  }
-  */
+ 
   @ViewChild('dynamicComp', { read: ViewContainerRef })
   private viewRef!: ViewContainerRef;
   private componentRef!: ComponentRef<ArbusComponent>;
 
   
-
   Load() {
     this.viewRef.clear();
     this.componentRef = this.viewRef.createComponent(ArbusComponent);
